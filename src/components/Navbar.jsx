98b0501c0779f41c8b6259f5d7b381ad0be3efbc
prathamesh <shelke.prathamesh0012@gmail.com>
2023-08-29 12:01:22 +0530

    modified:   src/App.css 	modified:   src/components/Navbar.css 	modified:   src/components/Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from 'react';
+import './Navbar.css'
 
 const Navbar = () => {
   return (
     <div>
+      <div className="nav">
       <h1>TRAVEL.</h1>
       <div className="bar">
         <div className="bar1"></div>
         <div className="bar2"></div>
+      </div>
       </div>
     </div>
   )
