0f9db54a15371a1e20ed6d98be3dfcf8a0855d80
prathamesh <shelke.prathamesh0012@gmail.com>
2023-09-06 16:05:19 +0530

    modified:   src/components/Home.css 	modified:   src/components/Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,52 +1,77 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Home.css';
-import  mumbai from "../assets/desktop-wallpaper-bandra-worli-sea-link-mumbai-skyline 1.jpg"
-import delhi from "../assets/1167248 1.jpg"
-import himalayas from "../assets/nepal-himalayas-mountains-nature-wallpaper-preview 1.png"
+import mumbaiImage from "../assets/desktop-wallpaper-bandra-worli-sea-link-mumbai-skyline 1.jpg";
+import delhiImage from "../assets/1167248 1.jpg";
+import himalayasImage from "../assets/nepal-himalayas-mountains-nature-wallpaper-preview 1.png";
+
 const Home = () => {
-   const active =0
+  const [index, setIndex] = useState(0);
+  const [slideDirection, setSlideDirection] = useState('slide-in-left');
 
-   const tags = document.querySelectorAll(".tagcontainer h1");
-   const img = document.querySelectorAll(".imgcontainer img")
+  const tags = ["Mumbai.", "Delhi.", "Himalayas."];
+  const images = [mumbaiImage, delhiImage, himalayasImage];
+
+  useEffect(() => {
    
-   const gotoprev = ()=>{
+    if (index === 0) {
+      setSlideDirection('slide-in-left');
+    } else if (index === images.length - 1) {
+      setSlideDirection('slide-in-right');
+    } else {
+      setSlideDirection('slide-in-left');
+    }
+  }, [index, images.length]);
 
-   }
-const gotonext = ()=>{
-  
-}
-  
- 
+  const goToNext = () => {
+    if (index < tags.length - 1) {
+      setIndex(index + 1);
+    }
+  };
+
+  const goToPrev = () => {
+    if (index > 0) {
+      setIndex(index - 1);
+    }
+  };
+
   return (
-    
     <div>
       <div className="maincontainer">
-      <div className="container">
-        <div className="imgcontainer">
-        <img src={mumbai} alt="" />
-        <img src={delhi} alt="" />
-        <img src={himalayas} alt="" />
-      </div>
-      <div className="tagcontainer">
-          <h1>MUMBAI.</h1>
-          <h1>DELHI.</h1>
-          <h1>HIMALAYAS.</h1>
+        <div className={`container ${slideDirection}`}>
+          <div className="imgcontainer">
+            {images.map((img, i) => (
+              <img
+                key={i}
+                src={img}
+                alt={`Image ${i + 1}`}
+                className={`image ${index === i ? "active" : ""}`}
+              />
+            ))}
+          </div>
+          <div className="tagcontainer">
+            {tags.map((tag, i) => (
+              <h1
+                key={i}
+                className={`tag ${index === i ? "active" : ""}`}
+              >
+                {tag}
+              </h1>
+            ))}
+          </div>
         </div>
-        
-      </div>
-      <div className="paracontainer">
+        <div className="paracontainer">
           <p>"Mumbai, the bustling city of dreams where tradition meets modernity."</p>
           <p>"Delhi: where history breathes amidst modernity's pulse."</p>
           <p>"The Himalayas: where Earth touches the sky in a sublime embrace."</p>
         </div>
-    </div>
-    <div className="footer">
-      <h1 onClick={gotoprev}>PREV</h1>
-      <p>Embarking on journeys, we unwrap the gift of the world, finding pieces of ourselves scattered in every new place and person we encounter.</p>
-      <h1 onClick={gotonext}>NEXT</h1>
-    </div>
+      </div>
+      <div className="footer">
+        <h1 onClick={goToPrev}>PREV</h1>
+        <p>Embarking on journeys, we unwrap the gift of the world, finding pieces of ourselves scattered in every new place and person we encounter.</p>
+        <h1 onClick={goToNext}>NEXT</h1>
+      </div>
     </div>
   )
 }
 
-export default Home
+export default Home;
